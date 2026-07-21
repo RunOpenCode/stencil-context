a93db24b70e6b70066ad47eefd8b222847971226
Nikola Svitlica a.k.a TheCelavi <thecelavi@gmail.com>
2026-07-21 13:30:23 +0200

    Renamed function named, improved docs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { createContext } from '@lit/context';
 
-export *                         from './context/provide-context';
-export *                         from './context/decorator/provide';
-export *                         from './context/decorator/consume';
-export { default as initialize } from './initialize-context';
+export *                                from './context/provide-context';
+export *                                from './context/decorator/provide';
+export *                                from './context/decorator/consume';
+export { default as initializeContext } from './initialize-context';
 // Re-export.
-export { createContext }
+export { createContext };
